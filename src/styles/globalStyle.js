aa61d4486bed8e7dab5ce7f4f62ac531a654e9ac
ferueda <fe.rueda@outlook.com>
2020-10-22 19:00:31 -0300

    Create globalStyles.js
    Created the styled-components GlobalStyles

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
+
+  body::-webkit-scrollbar {
+    display: none;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+`;
+
+export default GlobalStyle;
