24779e4a54a6bde6559bc0a906bb596b4897516d
ferueda <fe.rueda@outlook.com>
2020-10-22 19:01:03 -0300

    Create spotify.js
    Created the file that contains the logic and communication with the Spotify API

---
+++ src/services/spotify.js
@@ -0,0 +1,28 @@
+import { clientId } from '../utils/config';
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+const redirectUri = 'http://localhost:3000/';
+
+const scopes = [
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-read-playback-state',
+  'user-top-read',
+  'user-modify-playback-state',
+];
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  '%20',
+)}&response_type=token&show_dialog=true`;
+
+export function getTokenFromUrl() {
+  return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((acc, item) => {
+      let parts = item.split('=');
+      acc[parts[0]] = decodeURIComponent(parts[1]);
+      return acc;
+    }, {});
+}
