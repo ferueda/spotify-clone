6eea75d071dcad8fbcbd21f0727905ce6ceb02e9
ferueda <fe.rueda@outlook.com>
2020-10-22 19:00:02 -0300

    Create theme.js
    Created the file that contains the theme object

---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  color: {
+    mainGreen: '#1db954',
+    darkGreen: '#15883e',
+    lightGray: '#b3b3b3',
+    player: {
+      text: '#fff',
+      sidebarBg: '#040404',
+      bodyBg: '#121212',
+      // bodyBg: 'linear-gradient(rgba(91, 87, 115), #000)',
+      footerBg: '#282828',
+    },
+    playlistCard: {
+      bg: '#181818',
+      bgHover: '#282828',
+    },
+  },
+};
+
+export default theme;
