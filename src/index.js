d1fb242375489ce7841493755b557eaca791aa6d
ferueda <fe.rueda@outlook.com>
2020-10-22 18:57:42 -0300

    Add styled-components theme and reducer architecture

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+
 import App from './App';
+
+import StateProvider from './context/StateProvider';
+import stateReducer, { initialState } from './context/stateReducer';
+
+import GlobalStyle from './styles/globalStyle';
+import theme from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+
+      <StateProvider initialState={initialState} reducer={stateReducer}>
+        <App />
+      </StateProvider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
