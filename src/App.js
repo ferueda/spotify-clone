42b4432208fe43a0e7271340fd3037c55d13d7ee
ferueda <fe.rueda@outlook.com>
2020-11-10 22:14:20 -0300

    Update App.js
    Added react router
    Added routes handlers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
+import styled from 'styled-components';
+import { Switch, Route, Redirect, useHistory } from 'react-router-dom';
 
 import { getTokenFromUrl } from './services/spotify';
 import { useStateContext } from './context/StateProvider';
 import { actions } from './context/stateReducer';
+import * as ROUTES from './utils/routes';
 
-import Login from './components/Login';
-import Player from './components/Player';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Login from './pages/Login';
+
+import Sidebar from './components/Sidebar/Sidebar';
+import Footer from './components/Footer/Footer';
 
 const spotify = new SpotifyWebApi();
 
+const Main = styled.main`
+  display: flex;
+`;
+
 function App() {
   const [{ accessToken }, dispatch] = useStateContext();
+  const history = useHistory();
 
   React.useEffect(() => {
     const hash = getTokenFromUrl();
-    window.location.hash = '';
+    history.push('/');
 
     const _token = hash.access_token;
 
@@ -32,13 +44,29 @@ function App() {
         dispatch({ type: actions.SET_PLAYLISTS, playlists });
       });
     }
-  }, [accessToken, dispatch]);
+  }, [accessToken, dispatch, history]);
 
   if (accessToken === null || accessToken === undefined) {
     return <Login />;
   }
 
-  return <Player spotify={spotify} />;
+  return (
+    <Main>
+      <Sidebar />
+
+      <Switch>
+        <Route exact path={ROUTES.HOME}>
+          <Home spotify={spotify} />
+        </Route>
+
+        <Route exact path={ROUTES.SEARCH}>
+          <Search />
+        </Route>
+      </Switch>
+
+      <Footer />
+    </Main>
+  );
 }
 
 export default App;
