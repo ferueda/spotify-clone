96a9bf4a478bf876a56c746f7f376cc9282d951c
ferueda <fe.rueda@outlook.com>
2020-11-10 22:15:50 -0300

    Update Sidebar.js
    Added scrollbar styles
    Added state based on location for selected location

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,17 +4,51 @@ import SidebarOption from './SidebarOption';
 import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
 import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import { Link, useLocation } from 'react-router-dom';
+
+import { useStateContext } from '../../context/StateProvider';
+import * as ROUTES from '../../utils/routes';
 
 const Container = styled.nav`
-  height: 100vh;
+  height: calc(100vh - 65px);
+  overflow-y: scroll;
   width: 260px;
   background-color: ${({ theme }) => theme.color.player.sidebarBg};
   color: ${({ theme }) => theme.color.player.text};
   padding: 14px 10px 0 10px;
+
+  &::-webkit-scrollbar-track {
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.color.player.bodyBg};
+  }
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    background-color: ${({ theme }) => theme.color.player.bodyBg};
+  }
+
+  &::-webkit-scrollbar-thumb {
+    padding: 1px;
+    border-radius: 100px;
+    background: rgba(255, 255, 255, 0.3);
+  }
+
+  & a {
+    text-decoration: none;
+  }
 `;
 
 const LogoContainer = styled.div`
   height: 65px;
+`;
+
+const Logo = styled.img`
+  object-fit: contain;
+  height: 100%;
+  margin-right: auto;
+`;
+
+const MenuContainer = styled.div`
   color: ${({ theme }) => theme.color.lightGray};
 
   & > hr {
@@ -24,46 +58,63 @@ const LogoContainer = styled.div`
   }
 `;
 
-const Logo = styled.img`
-  object-fit: contain;
-  height: 100%;
-  margin-right: auto;
-`;
-
 const Playlist = styled.strong`
   margin-left: 10px;
   padding: 5px;
   font-size: 12px;
 `;
 
-function Sidebar({ playlists }) {
+function Sidebar() {
+  const { pathname } = useLocation();
+  const [selected, setSelected] = React.useState(pathname);
+  const [{ playlists }] = useStateContext();
+
+  React.useEffect(() => {
+    setSelected(pathname);
+  }, [pathname]);
+
   return (
     <Container>
-      <LogoContainer>
-        <a href="/">
-          <Logo
-            src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-            alt="spotify logo"
-          />
-        </a>
+      <div>
+        <LogoContainer>
+          <a href="/">
+            <Logo
+              src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+              alt="spotify logo"
+            />
+          </a>
+        </LogoContainer>
 
-        <SidebarOption title="Home" Icon={HomeIcon} />
-        <SidebarOption title="Search" Icon={SearchIcon} />
-        <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
+        <MenuContainer>
+          <Link to={ROUTES.HOME}>
+            <SidebarOption title="Home" Icon={HomeIcon} selected={selected === ROUTES.HOME} />
+          </Link>
 
-        <br />
+          <Link to={ROUTES.SEARCH}>
+            <SidebarOption title="Search" Icon={SearchIcon} selected={selected === ROUTES.SEARCH} />
+          </Link>
 
-        <Playlist>PLAYLISTS</Playlist>
+          <Link to={`${ROUTES.LIBRARY}/playlists`}>
+            <SidebarOption
+              title="Your Library"
+              Icon={LibraryMusicIcon}
+              selected={selected.includes(ROUTES.LIBRARY)}
+            />
+          </Link>
 
-        <hr />
+          <br />
 
-        {
-          //CEATE NEW COMPONENT FOR PLAYLISTS ITEMS
-        }
-        {playlists?.items?.slice(0, 15).map((playlist) => (
-          <SidebarOption key={playlist.id} title={playlist.name} />
-        ))}
-      </LogoContainer>
+          <Playlist>PLAYLISTS</Playlist>
+
+          <hr />
+        </MenuContainer>
+      </div>
+      {
+        //CEATE NEW COMPONENT FOR PLAYLISTS ITEMS
+      }
+      {playlists?.map((playlist) => (
+        <SidebarOption key={playlist.id} title={playlist.name} />
+      ))}
     </Container>
   );
 }
