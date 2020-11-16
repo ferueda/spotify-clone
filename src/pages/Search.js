ec4ad74153961b11635fcdeb817e763c22d888be
ferueda <fe.rueda@outlook.com>
2020-11-15 21:33:50 -0300

    Update Search.js
    Added Body and Header to the Search Page Component

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function Search() {
-  return <div>Search PAGE</div>;
+import Body from '../components/Body/Body';
+import Header from '../components/Body/Header';
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+function Search({ spotify }) {
+  return (
+    <Container>
+      <Body>
+        <Header spotify={spotify} withSearch />
+      </Body>
+    </Container>
+  );
 }
 
 export default Search;
