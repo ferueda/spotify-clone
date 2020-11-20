cc48a1fc81f9097e1dee539cc81deab720bb5289
ferueda <fe.rueda@outlook.com>
2020-11-19 21:28:43 -0300

    Update spotify.js
    Refactored the function that gets the token from location hash

--- src/services/spotify.js
+++ src/services/spotify.js
@@ -1,8 +1,8 @@
 import { clientId } from '../utils/config';
 
-const authEndpoint = 'https://accounts.spotify.com/authorize';
+const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 
-const redirectUri = 'http://localhost:5001/spotify-clone-88dae/us-central1/api/auth/';
+const REDIRECT_URI = 'http://localhost:5001/spotify-clone-88dae/us-central1/api/auth/';
 
 const scopes = [
   'user-read-currently-playing',
@@ -12,7 +12,7 @@ const scopes = [
   'user-modify-playback-state',
 ];
 
-export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+export const accessUrl = `${AUTH_ENDPOINT}?client_id=${clientId}&redirect_uri=${REDIRECT_URI}&scope=${scopes.join(
   '%20',
 )}&response_type=code&show_dialog=true`;
 
@@ -20,13 +20,5 @@ export function getTokenFromUrl() {
   const hash = window.location.hash;
   if (!hash) return null;
 
-  console.log(hash);
-  return window.location.hash
-    .substring(1)
-    .split('&')
-    .reduce((acc, item) => {
-      let parts = item.split('=');
-      acc[parts[0]] = decodeURIComponent(parts[1]);
-      return acc;
-    }, {});
+  return hash.split('token=')[1];
 }
