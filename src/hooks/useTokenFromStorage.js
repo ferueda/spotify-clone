f9fe6635152a095f7aaf173093c23df7743daa81
ferueda <fe.rueda@outlook.com>
2020-11-20 19:56:07 -0300

    Update useTokenFromStorage.js
    Moved the logic that gets the hash from URL from App into useTokenFromStorage hook.

--- src/hooks/useTokenFromStorage.js
+++ src/hooks/useTokenFromStorage.js
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { getTokenFromUrl } from '../services/spotify';
 
 function useTokenFromStorage() {
   const [token, setToken] = React.useState(() => {
@@ -11,8 +14,21 @@ function useTokenFromStorage() {
     return null;
   });
 
+  const history = useHistory();
+
   React.useEffect(() => {
-    if (token) {
+    const hash = getTokenFromUrl();
+
+    if (!token && hash) {
+      history.replace('/');
+      setToken(hash);
+    }
+  }, [history, token]);
+
+  React.useEffect(() => {
+    if (token === null) {
+      window.localStorage.removeItem('token');
+    } else {
       window.localStorage.setItem('token', JSON.stringify(token));
     }
   }, [token]);
