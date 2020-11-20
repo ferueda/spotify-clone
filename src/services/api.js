6f4ff63bee25776cf5f026b819eb4064ad8e7e7e
ferueda <fe.rueda@outlook.com>
2020-11-19 21:29:09 -0300

    Create api.js
    Created the file that contains api functions

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+const BASE_ENDPOINT = 'http://localhost:5001/spotify-clone-88dae/us-central1/api';
+
+const ENDPOINTS = {
+  getUser: '/spotify',
+  auth: '/auth',
+};
+
+export async function getSpotify(token) {
+  const url = `${BASE_ENDPOINT}${ENDPOINTS.getUser}?refresh_token=${token}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+}
