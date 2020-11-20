fe56492cdf81fa46845e9144b771bb540b920567
ferueda <fe.rueda@outlook.com>
2020-11-20 19:57:25 -0300

    Refactor useSpotify.js
    Moved useTokenFromStorage hook into useSpotify from App
    Added various playlists states
    Improved server error handling

--- src/hooks/useSpotify.js
+++ src/hooks/useSpotify.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { getSpotify } from '../services/api';
 
-function useSpotify(token) {
+import { getSpotify } from '../services/api';
+import useTokenFromStorage from './useTokenFromStorage';
+
+function useSpotify() {
   const [user, setUser] = React.useState(null);
   const [playlists, setPlaylists] = React.useState([]);
+  const [featured, setFeatured] = React.useState([]);
+  const [topArtistsLong, setTopArtistsLong] = React.useState([]);
+  const [topArtistsShort, setTopArtistsShort] = React.useState([]);
+  const [newReleases, setNewReleases] = React.useState([]);
   const [status, setStatus] = React.useState('idle');
   const [error, setError] = React.useState(null);
+
+  const [token, setToken] = useTokenFromStorage();
 
   const isLoading = status === 'idle' || status === 'pending';
 
@@ -18,20 +26,33 @@ function useSpotify(token) {
         .then((data) => {
           setUser(data.user);
           setPlaylists(data.playlists);
+          setFeatured(data.featured);
+          setTopArtistsLong(data.topArtistsLong);
+          setTopArtistsShort(data.topArtistsShort);
+          setNewReleases(data.newReleases);
           setStatus('resolved');
         })
         .catch((error) => {
           setError(error);
           setStatus('rejected');
+
+          if (error.message === 'Unauthorized') {
+            setToken(null);
+          }
         });
     }
-  }, [token]);
+  }, [token, setToken]);
 
   return {
     user,
     playlists,
+    featured,
+    topArtistsLong,
+    topArtistsShort,
+    newReleases,
     error,
     isLoading,
+    token,
   };
 }
 
