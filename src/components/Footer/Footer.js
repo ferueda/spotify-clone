724a4429963d4e4665bbc9b4a760d35ad6dc0d3e
ferueda <fe.rueda@outlook.com>
2020-11-26 18:51:25 -0300

    Refactor Footer.js
    Extracted the PlayerControls into its own component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-import SkipNextIcon from '@material-ui/icons/SkipNext';
-import ShuffleIcon from '@material-ui/icons/Shuffle';
-import RepeatIcon from '@material-ui/icons/Repeat';
-import VolumeControls from './VolumeConrols';
+import VolumeControls from './VolumeControls';
+import PlayerControls from './PlayerControls';
 
 const Container = styled.div`
   position: fixed;
@@ -22,35 +18,10 @@ const Container = styled.div`
 `;
 
 const SongDetailsContainer = styled.div`
-  flex: 0.3;
   display: flex;
   align-items: center;
   color: ${({ theme }) => theme.color.lightGray};
-  width: 300px;
-`;
-
-const PlayerControls = styled.div`
-  flex: 0.4;
-  display: flex;
-  padding: 0 100px;
-  color: ${({ theme }) => theme.color.lightGray};
-  align-items: center;
-  justify-content: space-between;
-  width: 300px;
-`;
-
-const IconContainer = styled.div`
-  color: ${({ active, theme }) => (active ? theme.color.darkGreen : theme.color.lightGray)};
-  &:hover {
-    color: ${({ active, theme }) => (active ? theme.color.mainGreen : theme.color.player.text)};
-  }
-`;
-
-const PlayIconContainer = styled(IconContainer)`
-  transition-duration: 200ms;
-  &:hover {
-    transform: scale(1.1);
-  }
+  min-width: 300px;
 `;
 
 const AlbumImg = styled.img`
@@ -60,43 +31,38 @@ const AlbumImg = styled.img`
   object-fit: contain;
 `;
 
-const AlbumText = styled.p`
+const Track = styled.p`
   color: white;
   font-size: 14px;
 `;
 
-const ArtistText = styled.p`
+const Artist = styled.p`
   font-size: 12px;
 `;
 
-function Footer() {
+function Footer({ spotify }) {
+  const { recentlyPlayed } = spotify;
+
+  const track = recentlyPlayed[0]?.track.name;
+  const artist = recentlyPlayed[0]?.track.album.artists[0].name;
+  const albumImg = recentlyPlayed[0]?.track.album.images[2].url;
+  const album = recentlyPlayed[0]?.track.album.name;
+
   return (
     <Container>
       <SongDetailsContainer>
-        <AlbumImg src="" alt="" />
-        <div>
-          <AlbumText>Album</AlbumText>
-          <ArtistText>Singer</ArtistText>
-        </div>
+        {track ? (
+          <>
+            <AlbumImg src={albumImg} alt={`${album} album image`} />
+            <div>
+              <Track>{track}</Track>
+              <Artist>{artist}</Artist>
+            </div>
+          </>
+        ) : null}
       </SongDetailsContainer>
 
-      <PlayerControls>
-        <IconContainer>
-          <ShuffleIcon />
-        </IconContainer>
-        <IconContainer>
-          <SkipPreviousIcon />
-        </IconContainer>
-        <PlayIconContainer>
-          <PlayCircleOutlineIcon fontSize="large" />
-        </PlayIconContainer>
-        <IconContainer>
-          <SkipNextIcon />
-        </IconContainer>
-        <IconContainer>
-          <RepeatIcon />
-        </IconContainer>
-      </PlayerControls>
+      <PlayerControls currentTrack={recentlyPlayed[0]} />
 
       <VolumeControls />
     </Container>
